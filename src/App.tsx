7657170f251e6f42d523cce6c570d754f5027a11
Miko <barreyrojohnmiko@gmail.com>
2023-03-04 18:04:14 +0800

    installed react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
-import React, { useState } from "react";
-import Card from "./views/Card";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import Dump from "./components/Dump";
+import Home from "./components/Home";
 
 import "./App.css";
 
 const App = () => {
-  const [cardData, setCardData] = useState({
-    fName: "Sofia Noreen",
-    lName: "Esposo",
-    age: 22,
-  });
-
   return (
     <div className="main-component">
-      <Card {...cardData} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/dump" element={<Dump />} />
+      </Routes>
     </div>
   );
 };
