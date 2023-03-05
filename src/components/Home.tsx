3ea0bd34f239e1954509d0897b3b49a8db2b5faf
Miko <barreyrojohnmiko@gmail.com>
2023-03-05 11:01:03 +0800

    updated header

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,8 +1,9 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 import "./Home.css";
 import Card from "../views/Card";
+import AnimatedButton from "../views/AnimatedButton";
 
 const Home = () => {
   const [cardData, setCardData] = useState({
@@ -19,15 +20,29 @@ const Home = () => {
       <div className="header">
         <div className="header-logo">MIKO</div>
         <div className="header-menu">
-          <Link to="/about" className="btn-about-link">
-            <button className="header-btn-text-normal btn-about">About</button>
-          </Link>
-          <Link to="/portfolio" className="btn-resume-link">
-            <button className="header-btn-text-normal btn-resume">Portfolio</button>
-          </Link>
-          <Link to="/contact" className="btn-contact-link">
-            <button className="header-btn-text-special btn-contact">Contact</button>
-          </Link>
+          <div className="header-btn-text-spacing">
+            <button className="header-btn-text btn-border">About</button>
+          </div>
+          <div className="header-btn-text-spacing">
+            <button className="header-btn-text btn-border">Experience</button>
+          </div>
+          <div className="header-btn-text-spacing">
+            <button className="header-btn-text btn-border">Works</button>
+          </div>
+          <div className="btn-contact-spacing">
+            <AnimatedButton />
+          </div>
+        </div>
+      </div>
+
+      <div className="body">
+        <div className="about-dev">
+          <div className="job-title">Front-end Developer</div>
+          <div className="job-description">
+            Detail-oriented front-end developer who is passionate about crafting
+            engaging user experiences. I take pride in bringing your ideas to
+            life with clean and efficient code.
+          </div>
         </div>
       </div>
     </div>
---
+++ src/views/AnimatedButton.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import "./AnimatedButton.css";
+
+const AnimatedButton = () => {
+  return (
+    <div>
+      <button className="animated-button">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span> Contact Me
+      </button>
+    </div>
+  );
+};
+
+export default AnimatedButton;
