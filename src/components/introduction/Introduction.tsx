a82fbb12ae3292db71f7c0e885a71747d3040b1c
Miko <barreyrojohnmiko@gmail.com>
2023-11-05 10:35:26 +0800

    ui update

--- src/components/introduction/Introduction.tsx
+++ src/components/introduction/Introduction.tsx
@@ -6,7 +6,7 @@ const Introduction = () => {
       <div className="greetings"> Hello, my name is </div>
       <div className="full-name"> Barreyro, John Miko</div>
       <div className="job-description">
-        I'm a web and mobile front-end developer who is passionate about
+        I'm a software developer who is passionate about
         crafting engaging user experiences. I take pride in bringing your ideas
         to life with clean and efficient code.
       </div>
