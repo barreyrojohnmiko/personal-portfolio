f7d12e9eb8d2c0e57be7ee00ddff22a3bbd172e2
Miko Barreyro <110441796+barreyrojohnmiko@users.noreply.github.com>
2023-11-22 11:30:14 +0800

    removed header

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -6,15 +6,19 @@ import Header from "../../views/header/Header";
 import Sidebar from "../../views/sidebar/Sidebar";
 import About from "../about/About";
 import Contact from "../contact/Contact";
-import Introduction from "../introduction/Introduction";
 import Works from "../works/Works";
 
 import { useDispatch, useSelector } from "react-redux";
-import { setIsMobileView, setIsNavbarOpen } from "../../redux/components/home/action";
+import {
+  setIsMobileView,
+  setIsNavbarOpen,
+} from "../../redux/components/home/action";
 
 const Home = () => {
   const dispatch: any = useDispatch();
-  const { isNavbarOpen, isMobileView } = useSelector((state: any) => state.homeReducers);
+  const { isNavbarOpen, isMobileView } = useSelector(
+    (state: any) => state.homeReducers
+  );
 
   const aboutSectionRef = useRef<HTMLDivElement>(null);
   const contactSectionRef = useRef<HTMLDivElement>(null);
@@ -52,8 +56,11 @@ const Home = () => {
   };
 
   const handleResumeClick = () => {
-    window.open('https://drive.google.com/file/d/1srIhNxPqkJE3tgj0nWWtpwm6zG7eByqS/view?usp=sharinga', '_blank');
-  }
+    window.open(
+      "https://drive.google.com/file/d/1srIhNxPqkJE3tgj0nWWtpwm6zG7eByqS/view?usp=sharinga",
+      "_blank"
+    );
+  };
 
   const toggleNavbar = () => {
     dispatch(setIsNavbarOpen(!isNavbarOpen));
@@ -79,6 +86,7 @@ const Home = () => {
       <div className="sidebar-section">
         <Sidebar />
       </div>
+
       <div className="content-main-section">
         <div className="header-section">
           <Header
@@ -89,10 +97,20 @@ const Home = () => {
             toggleNavbar={toggleNavbar}
           />
         </div>
+        
         <div className={`content-sub-section ${isNavbarOpen ? "open" : ""}`}>
           <div className="introduction-section">
-            <Introduction />
+            <div className="introduction-main-container">
+              <div className="greetings"> Hello, my name is </div>
+              <div className="full-name"> Barreyro, John Miko</div>
+              <div className="job-description">
+                I'm a software developer who is passionate about crafting
+                engaging user experiences. I take pride in bringing your ideas
+                to life with clean and efficient code.
+              </div>
+            </div>
           </div>
+
           <div className="about-section content-section-padding">
             <About aboutSectionRef={aboutSectionRef} />
           </div>
