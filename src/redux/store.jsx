091bb9f238651b40627ff820a767257bf25ea1fb
Miko <barreyrojohnmiko@gmail.com>
2024-02-25 22:10:51 +0800

    header css to scss

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -2,12 +2,10 @@ import { applyMiddleware, combineReducers, createStore } from "redux";
 import thunk from "redux-thunk";
 
 import homeReducers from "./components/home/reducers";
-import projectsReducers from "./components/projects/reducers";
 import headerReducers from "./views/header/reducers";
 
 const rootReducer = combineReducers({
   homeReducers,
-  projectsReducers,
   headerReducers,
 });
 
